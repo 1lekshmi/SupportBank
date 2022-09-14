c80b104215e9e08d192cb0beee9b5a9c9289ff5e
Lekshmi Sudhikumar <skumarlekshmi@gmail.com>
2022-09-14 19:53:10 +0100

    Beginning of Part 1

---
+++ src/main/java/training/supportbank/Account.java
@@ -0,0 +1,20 @@
+package training.supportbank;
+
+public class Account {
+
+
+//    fields
+    private String name;
+    private double balance;
+
+//    constructor
+    public Account(String name){
+        this.name = name;
+
+    }
+
+//    public void setName(String newName){
+//
+//
+//    }
+}
--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,28 @@
 package training.supportbank;
+import java.util.Scanner;
+import com.opencsv.CSVReader;
+import org.apache.commons.text.WordUtils;
 
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
-        System.out.println("Test!");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("If you want to list all the accounts, enter \"List All \".");
+        System.out.println("If you want account details on a certain account, enter \"List <name>\" and replace <name> with the name of the account holder. " );;
+        String command = WordUtils.capitalizeFully(input.nextLine());
+
+
+        if (command.equals("List All")){
+            // print out name and the balance
+
+
+        } else {
+            //extracting the name entered
+            String[] commands = command.split(" ");
+            String inputtedName = commands[1];
+
+            // need to insert a function that will print out all the transactions, with the date and narrative.
+        }
+
     }
 }
